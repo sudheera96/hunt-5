1be5042e78371811848fe78421e453024da56961
Ravichanderreddy-goli <60166223+Ravichanderreddy-goli@users.noreply.github.com>
2020-11-13 14:14:39 -0600

    Update competition.js
    updated the code in competition

--- models/competition.js
+++ models/competition.js
@@ -1,1 +1,48 @@
+/**
+ *  Team model
+ *  Describes the characteristics of each attribute in a Team resource.
+ *
+ * @author Ravichander Reddy Goli
+ *
+ * For more information about defining sequelize models, see
+ * https://sequelize.org/v5/manual/data-types.html
+ * https://sequelize.org/master/manual/validations-and-constraints.html
+ *
+ * For validators see: https://github.com/validatorjs/validator.js
+ *
+ */
+// Export a function that defines the model.
+// It automatically receives the Sequelize connection parameter.
 
+module.exports = (db, DataTypes) => {
+    db.define('competition', {
+      // sqlite creates a rowid attribute automatically
+      competitionName: {
+        type: DataTypes.STRING(30),
+        unique: true,
+        required: true,
+        allowNull: false,
+        defaultValue: 'Team',
+        validate: {
+          is: {
+            args: /^[A-Za-z]+$/i, // matches a RegExp
+            msg: 'Name is only letters, no spaces or punctuation.',
+          },
+          notNull: {
+            args: true,
+            msg: 'Name cannot be null.',
+          },
+          notEmpty: {
+            args: true, // RegExp- only letters, no spaces
+            msg: 'Name cannot be empty.',
+          },
+          max: {
+            args: [30],
+            msg: 'Name is limited to 32 characters.',
+          },
+          min: {
+            args: [3],
+            msg: 'Name must be at least 3 characters.',
+          },
+        },
+      },
