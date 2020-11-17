3575c664b6c259de3aa7ec0e2b5ac9b3fc4e66c6
gundu pooja <gundupooja1995@gmail.com>
2020-11-16 20:38:06 -0600

    added quest model

---
+++ models/quest.js
@@ -0,0 +1,65 @@
+
+module.exports = (db, DataTypes) => {
+    db.define('quest', {
+       questId: {
+           type: DataTypes.INTEGER,
+           primaryKey: true,
+           allowIncrement: true
+       },
+       questName: {
+           type: DataTypes.STRING,
+           validate: {
+             len: [3,10]
+           },
+           allowNull: false,
+           unique: {
+            args: 'questName',
+            msg: 'The questName is already taken!'
+           },
+           return: true
+       },
+       creatorUserId: {
+        type: DataTypes.INTEGER,
+        // references: {
+        //     model: "user",
+        //     primaryKey: "userid"},
+
+        },
+        dateTimeCreated: {
+            type: DataTypes.DATE,
+            get() {
+                return moment(this.getDataValue('dateTimeCreated')).format('MM/DD/YYYY');
+            }
+        },
+        dateTimeLastEdited: {
+            type: DataTypes.DATE,
+            get(){
+                return moment(this.getDataValue('dateTimeLastEdited')).format('MM/DD/YYYY');
+
+            }
+
+        },
+        latitude: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: null,
+            validate: { min: -90, max: 90 }
+          },
+          longitude: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: null,
+            validate: { min: -180, max: 180 }
+          },
+        }, {
+          validate: {
+            bothCoordsOrNone() {
+              if ((this.latitude === null) !== (this.longitude === null)) {
+                throw new Error('Require either both latitude and longitude or neither')
+              }
+            }
+          }
+
+    });
+  //  module.exports = quest;
+}
