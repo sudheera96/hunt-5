c41634d018096f6097f104b29824f46ebc45b046
Saigorla <41150392+SaiGorla@users.noreply.github.com>
2020-11-16 22:53:54 -0600

    updated usercontroller.js about GET new User page

--- controllers/ClueController.js
+++ controllers/ClueController.js
@@ -37,3 +37,39 @@ exports.logout = function (req, res) {
     });
 }
 
+/* GET New User page. */
+exports.newuser = function (req, res) {
+    res.render('user/create', { title: 'Add New User' });
+};
+
+/* POST to Add User Service */
+//router.post('/adduser', function(req, res) {
+exports.adduser = function (req, res) {
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Submit to the DB
+    const userdata = {
+        username: userName,
+        email: userEmail
+    };
+
+    const newUser = new User(userdata);
+
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err)
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            console.log("Data Saved");
+            res.redirect("/user/index");
+        }
+    });
+
+};
+
