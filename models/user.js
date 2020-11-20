566c126f411e2d4b6bb31fd104c7be2a50f52588
gundu pooja <gundupooja1995@gmail.com>
2020-11-20 13:07:46 -0600

    updated routes and models files of users.js

--- models/user.js
+++ models/user.js
@@ -4,28 +4,39 @@
  *
  * @author Denise Case <dcase@nwmissouri.edu>
  *
- * For more information about defining sequelize models, see
+For more information about defining sequelize models, see
  * https://sequelize.org/v5/manual/data-types.html
+ * https://sequelize.org/master/manual/validations-and-constraints.html
+ *
+ * For validators see: https://github.com/validatorjs/validator.js
  *
  */
+// Export a function that defines the model.
+// It automatically receives the Sequelize connection parameter.
 
 module.exports = (db, DataTypes) => {
   db.define(
     'User',
     {
       email: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(100),
+        unique: true,
+        required: true,
+        allowNull: false,
+        defaultValue: 'me@nwmissouri.edu',
         validate: {
           isEmail: true,
         },
       },
       password: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(25),
+        required: true,
         allowNull: false,
       },
       last_login: DataTypes.DATE,
     },
-   
-);
-  // return User;
+    {
+      // Other model options go here
+    },
+  );
 };
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,43 @@
-const express = require('express');
+/**
+ * Route user requests to the correct controller function.
+ *
+ * A request includes:
+ *  an HTTP verb (e.g., get or post) AND
+ *  a URL endpoint (e.g., /create)
+ *
+ * Match each expeced verb + URL request
+ * with a custom function to handle it
+ *
+ * @author Denise Case <dcase@nwmissouri.edu>
+ */
 
-const router = express.Router();
+const router = require('express').Router();
+const controller = require('../controllers/userController.js');
 
-/* GET users listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
-});
+console.info('Starting user routing.');
+
+// -----------------------------------------------------------------------------
+// match each expeced verb + URL request
+// with a custom function to handle it
+// -----------------------------------------------------------------------------
+
+// handle requests for JSON (HTTP GET) - temporary
+
+router.get('/', controller.findAll);
+router.get('/findall', controller.findAll);
+
+// handle requests to perform update actions (HTTP POST)
+
+router.post('/login', controller.postLogin);
+router.post('/register', controller.postRegister);
+router.post('/forgot-password', controller.postForgotPassword);
+
+// handle requests for webpages (HTTP GET)
+
+router.get('/login', controller.showLogin);
+router.get('/register', controller.showRegister);
+router.get('/forgot-password', controller.showForgotPassword);
+
+console.info('Loaded user routes.');
 
 module.exports = router;
